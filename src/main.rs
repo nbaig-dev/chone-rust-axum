af7225513e0d69c650f80def39dd6f1e2f89400a
Nishat Baig <nbaig.dev@gmail.com>
2024-01-26 18:05:32 +0530

    Basic hello world route.

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+#![allow(unused)]
+
+use std::net::SocketAddr;
+use axum::response::Html;
+use axum::{Router, ServiceExt};
+use axum::routing::get;
+
+#[tokio::main]
+async fn main() {
+    let routes_hello = Router::new()
+        .route(
+            "/hello",
+            get(|| async {Html("Hello <strong>World!</strong>")}),
+        );
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+    println!("->> Listening on {addr} \n");
+    axum::Server::bind(&addr)
+        .serve(routes_hello.into_make_service())
+        .await
+        .unwrap();
+}
